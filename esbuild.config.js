c7dffd32166b8bfc097f14a433eb90c2d76d4d59
Manases <manasesmutembei11@gmail.com>
2025-02-13 11:53:31 +0300

    User app setup

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,14 @@
 const esbuild = require('esbuild');
 
 esbuild.build({
-    entryPoints: ['src/index.jsx'],
+    entryPoints: ['src/index.js'],
     bundle: true,
     outfile: 'dist/bundle.js',
     minify: true,
     sourcemap: true,
     target: ['es2015'],
     jsx: 'react-jsx',
+    jsx: "automatic",
+    loader: { '.js': 'jsx' }
+
 }).catch(() => process.exit(1));
